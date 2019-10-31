56b417e9920791ff155a4a9fa78218188e94abfb
Nikolay Klimov <krovoron@outlook.com>
2019-10-31 19:57:32 +0300

    COMMIT 31.10.2019_19:56

--- src/main/java/com/javamaster/controller/MainController.java
+++ src/main/java/com/javamaster/controller/MainController.java
@@ -1,30 +1,60 @@
 package com.javamaster.controller;
 
+import com.javamaster.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.*;
+import com.javamaster.service.UserServiceImple;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class MainController {
-
     //Мы могли бы расписать эти 2 маппинга отдельно, но смысла дублировать одинаковый код нет.
     // этот метод будет слушать запросы на "/" и "/index"
     /*@GetMapping(value = {"/", "/index"})
     public String getMainPage() {
         return "index";
     }*/
-    @RequestMapping("/login")
+    @Autowired
+    private UserServiceImple usi;
+    @RequestMapping(value="/login", method= RequestMethod.GET)
     public String getLogin(@RequestParam(value = "error", required = false) String error,
                            @RequestParam(value = "logout", required = false) String logout,
                            Model model) {
         model.addAttribute("error", error != null);
         model.addAttribute("logout", logout != null);
         return "login";
+    }
+    /*@PostMapping("/login")
+    public String postLogin() {
+        //
+        return "login";
+    }*/
+    @RequestMapping(value="/admin", method=RequestMethod.GET)
+    public String getAdminPageGet(Model model) throws SQLException {
+        List<User> userList = usi.getListUsers();
+        model.addAttribute("users", userList);
+        return "admin";
+    }
+    @RequestMapping(value="/admin", method=RequestMethod.POST)
+    public String getAdminPagePost(Model model, @RequestParam(value="ButtonName") String butname, HttpServletResponse resp) throws SQLException {
+        if(butname.equals("Delete_All_Users")) {
+            usi.cleanTable();
+        } else {
+            try {
+                int number = Integer.parseInt(butname);
+                usi.deleteId(number);
+                return "redirect:/admin";
+            } catch (Throwable throwable) {
+                System.out.println("throwable[Admin_doPost_deleteUser]: "+throwable.toString());
+            }
+        }
+        return "admin";
     }
 /*
     @GetMapping("/admin")
